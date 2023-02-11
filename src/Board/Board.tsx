4e9fee479a2510fbbd613622af38d179c8b02dc7
Maik <maikwegener@mailbox.org>
2023-02-12 00:53:04 +0100

    add calc for chance and totals

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -56,13 +56,13 @@ class Board extends Component<IProps, IState> {
             {this.renderSingleLabel("Three of a kind")}
             {this.renderSingleLabel("Four of a kind")}
             {this.renderSingleLabel("Full House")}
-            {/*{this.renderSingleLabel("Small Straight")}*/}
-            {/*{this.renderSingleLabel("Large Straight")}*/}
-            {/*{this.renderSingleLabel("Yahtzee")}*/}
-            {/*{this.renderSingleLabel("Chance")}*/}
-            {/*{this.renderSingleLabel("Total (Lower)")}*/}
-            {/*{this.renderSingleLabel("Total (Upper)")}*/}
-            {/*{this.renderSingleLabel("Grand Total")}*/}
+            {this.renderSingleLabel("Small Straight")}
+            {this.renderSingleLabel("Large Straight")}
+            {this.renderSingleLabel("Yahtzee")}
+            {this.renderSingleLabel("Chance")}
+            {this.renderSingleLabel("Total (Lower)")}
+            {this.renderSingleLabel("Total (Upper)")}
+            {this.renderSingleLabel("Grand Total")}
             </tbody>
         </table>)
     }
@@ -82,18 +82,25 @@ class Board extends Component<IProps, IState> {
         return (
             <table>
                 <tbody>
-                {this.renderSingleField(0, () => this.calcValueNumber(1))}
-                {this.renderSingleField(1, () => this.calcValueNumber(2))}
-                {this.renderSingleField(2, () => this.calcValueNumber(3))}
-                {this.renderSingleField(3, () => this.calcValueNumber(4))}
-                {this.renderSingleField(4, () => this.calcValueNumber(5))}
-                {this.renderSingleField(5, () => this.calcValueNumber(6))}
+                {this.renderSingleField(0, () => this.calcSumSingleNumber(1))}
+                {this.renderSingleField(1, () => this.calcSumSingleNumber(2))}
+                {this.renderSingleField(2, () => this.calcSumSingleNumber(3))}
+                {this.renderSingleField(3, () => this.calcSumSingleNumber(4))}
+                {this.renderSingleField(4, () => this.calcSumSingleNumber(5))}
+                {this.renderSingleField(5, () => this.calcSumSingleNumber(6))}
                 {this.renderSingleField(6, () => this.calcUpperTotalNoBonus(), true)}
                 {this.renderSingleField(7, () => this.calcBonus(), true)}
                 {this.renderSingleField(8, () => this.calcUpperBonus(), true)}
                 {this.renderSingleField(9, () => this.calcSumForXOfAKind(3))}
                 {this.renderSingleField(10, () => this.calcSumForXOfAKind(4))}
                 {this.renderSingleField(11, () => this.calcFullHouse())}
+                {this.renderSingleField(12, () => this.calcStraight())}
+                {this.renderSingleField(13, () => this.calcStraight(true))}
+                {this.renderSingleField(14, () => this.calcYahtzee())}
+                {this.renderSingleField(15, () => this.utilDiceSum(this.props.diceValues))}
+                {this.renderSingleField(16, () => this.calcLower(), true)}
+                {this.renderSingleField(17, () => this.calcUpperBonus(), true)}
+                {this.renderSingleField(18, () => this.calcGrandTotal(), true)}
                 </tbody>
             </table>
         )
@@ -112,6 +119,9 @@ class Board extends Component<IProps, IState> {
         this.lockField(6)
         this.lockField(7)
         this.lockField(8)
+        this.lockField(16)
+        this.lockField(17)
+        this.lockField(18)
     }
 
     lockField(i: number) {
@@ -126,7 +136,7 @@ class Board extends Component<IProps, IState> {
         this.setState({fieldValues: nextFieldValues})
     }
 
-    calcValueNumber(fieldNumber: number): number {
+    calcSumSingleNumber(fieldNumber: number): number {
         return this.props.diceValues
             .filter((value) => value === fieldNumber)
             .reduce((total, current) => total + current, 0)
@@ -149,9 +159,19 @@ class Board extends Component<IProps, IState> {
         return this.calcUpperTotalNoBonus() + this.calcBonus()
     }
 
+    calcLower(): number {
+        return this.state.fieldValues
+            .slice(9, 16)
+            .reduce((total, current) => total + current, 0)
+    }
+
+    calcGrandTotal(): number {
+        return this.calcUpperBonus() + this.calcLower()
+    }
+
     calcSumForXOfAKind(x: number): number {
         if (this.utilHasAtLeastXOfAKind(this.props.diceValues, x)) {
-            return this.props.diceValues.reduce((total, current) => total + current, 0)
+            return this.utilDiceSum(this.props.diceValues)
         }
         return 0
     }
@@ -165,8 +185,43 @@ class Board extends Component<IProps, IState> {
         return 0
     }
 
+    calcStraight(largeStraight: boolean = false): number {
+        const faceValues = Array.from({length: 6}, (x, i) => i + 1)
+        if (largeStraight) {
+            const largeStraights = faceValues.slice(0, 2).map((value, index) => {
+                return (this.props.diceValues.includes(faceValues[index])
+                    && this.props.diceValues.includes(faceValues[index + 1])
+                    && this.props.diceValues.includes(faceValues[index + 2])
+                    && this.props.diceValues.includes(faceValues[index + 3])
+                    && this.props.diceValues.includes(faceValues[index + 4])
+                )
+            })
+            return largeStraights.includes(true) ? 40 : 0;
+        } else {
+            const smallStraights = faceValues.slice(0, 3).map((value, index) => {
+                return (this.props.diceValues.includes(faceValues[index])
+                    && this.props.diceValues.includes(faceValues[index + 1])
+                    && this.props.diceValues.includes(faceValues[index + 2])
+                    && this.props.diceValues.includes(faceValues[index + 3])
+                )
+            })
+            return smallStraights.includes(true) ? 30 : 0;
+        }
+    }
+
+    calcYahtzee(): number {
+        if (this.utilHasExactXOfAKind(this.props.diceValues, 5)) {
+            return 50
+        }
+        return 0
+    }
+
 
     // TODO: move to utils.ts
+    utilDiceSum(diceValues: Array<number>): number {
+        return diceValues.reduce((total, current) => total + current, 0)
+    }
+
     utilHasAtLeastXOfAKind(diceValues: Array<number>, x: number): boolean {
         return Object.values(this.utilGetOccurrences(diceValues))
             .filter((value) => value >= x)
